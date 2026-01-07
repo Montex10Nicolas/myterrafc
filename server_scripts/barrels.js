12ee6f37bdf273f05235087cd3787a2c7921cf46
Nicolas10Montex <montexdev@protonmail.com>
2026-01-07 14:14:48 +0100

    Light gray dye from white/black and gray/white; Sealed recipe with ash soda for creating dye

--- server_scripts/barrels.js
+++ server_scripts/barrels.js
@@ -1,4 +1,5 @@
 // Priority: 0
+// Instant
 ServerEvents.recipes(event => {
   event.custom({
     type: "tfc:barrel_instant",
@@ -80,4 +81,58 @@ ServerEvents.recipes(event => {
       "amount": 1
     }
   });
+
+  event.custom({
+    type: "tfc:barrel_instant_fluid",
+    "primary_fluid": {
+      "ingredient": "tfc:white_dye",
+      "amount": 1
+    },
+    "added_fluid": {
+      "ingredient": "tfc:gray_dye",
+      "amount": 1
+    },
+    "output_fluid": {
+      "fluid": "tfc:light_gray_dye",
+      "amount": 1
+    }
+  });
+
+  event.custom({
+    type: "tfc:barrel_instant_fluid",
+    "primary_fluid": {
+      "ingredient": "tfc:white_dye",
+      "amount": 2
+    },
+    "added_fluid": {
+      "ingredient": "tfc:black_dye",
+      "amount": 1
+    },
+    "output_fluid": {
+      "fluid": "tfc:light_gray_dye",
+      "amount": 1
+    }
+  });
+});
+// Sealed
+ServerEvents.recipes(event => {
+  // IDK what would be the best thing to use instead of soda ash
+  colors.forEach(color => {
+    event.custom({
+      "type": "tfc:barrel_sealed",
+      "input_item": {
+        "ingredient": {
+          "item": "tfc:powder/soda_ash"
+        }
+      },
+      "input_fluid": {
+        "ingredient": `tfc:${color}_dye`,
+        "amount": 1000
+      },
+      "output_item": {
+        "item": `minecraft:${color}_dye`
+      },
+      "duration": 4000
+    })
+  })
 });
